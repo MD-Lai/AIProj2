8408289ba1bfbf5ccebe77c4d3e38959dfd2454d
MD-Lai <marvin.dhlai@gmail.com>
2017-05-11 18:17:33 +1000

    starting alpha beta pruner

---
+++ src/agents/mldz/ab/AB.java
@@ -0,0 +1,20 @@
+package agents.mldz.ab;
+
+import agents.mldz.Interplay;
+import aiproj.slider.Move;
+import board.Board;
+
+public class AB extends Interplay{
+	
+	public Move nextMove(){
+		
+		return null;
+	}
+	private int min(Board b, int alpha, int beta){
+		return 0;
+	}
+	
+	private int max(Board b, int alpha, int beta){
+		return 0;
+	}
+}
